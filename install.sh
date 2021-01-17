fd1a2bdaaee1920750daafca57beb2f5102146cd
SuperMrDoggyDog <treylewis717@gmail.com>
2021-01-17 13:10:14 -0600

    Updated install.sh and added snapappinstall.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd
 
-sudo pacman -S atom notepadqq lutris alacritty cheese chromium deadbeef udisks2 emacs gimp htop keepassxc virtualbox virt-manager qbittorent steam vim neovim exa vlc nano sudo audacity kdenlive discord libreoffice-fresh libnotify notification-daemon wine-staging winetricks
+sudo pacman -S atom notepadqq lutris alacritty cheese chromium deadbeef udisks2 emacs gimp htop keepassxc virtualbox virt-manager qbittorent steam vim neovim exa vlc nano sudo audacity kdenlive discord libreoffice-fresh libnotify notification-daemon wine-staging winetricks blender mpv
 
 git clone https://aur.archlinux.org/yay-bin.git
 
@@ -12,7 +12,11 @@ makepkg -si
 
 cd
 
-yay -S balena-etcher multimc5 multimc-curseforge minecraft-launcher nbtexplorer stacer-bin appimagelauncher-git
+yay -S balena-etcher multimc5 multimc-curseforge minecraft-launcher nbtexplorer stacer-bin appimagelauncher-git snapd
+
+sudo systemctl enable --now snapd.socket
+
+sudo ln -s /var/lib/snapd/snap /snap
 
 mkdir Applications
 
@@ -31,3 +35,7 @@ mv ./jetbrains-toolbox-1.19.7784/jetbrains-toolbox-1.19.7784.appimage ~/Applicat
 cd ~/Applications
 
 wget https://appoutlet.herokuapp.com/download/appimage
+
+cd
+
+echo -e "The system is about to reboot. Please close any running applications. When reboot is finished, run snapappinstall.sh"
