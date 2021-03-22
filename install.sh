0db4fb7aa565898e8906c2a997f8608a4a25564a
Trey <treylewis717@gmail.com>
2021-03-22 11:59:07 -0500

    Updated everything

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ makepkg -si
 paru -S nvidia xorg xorg-xinit awesomewm openbox tint2 xmonad xmonad-contrib leftwm xmobar lightdm lightdm-gtk-greeter light-locker polybar mesa qtile
 
 # Installing all programs needed
-paru -S alacritty audacity blender brave-bin cheese deadbeef discord emacs exa gimp htop kdenlive libnotify libreoffice-fresh lutris mpv nano neofetch nitrogen notepadqq notifications-daemon neovim picom polkit qbittorent steam sudo udisks2 vim virt-manager virtualbox vlc wine-staging winetricks multimc multimc-curseforge librewolf-bin snapd kvantum xterm pcmanfm mu kitty appimagelauncher ripgrep fd atom blueman bullshit chrome-remote-desktop chromium checkupdates-aur emby-theater-bin feh firefox fish geary heroic-games-launcher-bin minigalaxy minecraft-launcher network-manager-applet networkmanager newsflash nextcloud-client obs-studio openssh openvpn plex-media-player playerctl playonlinux pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-qt qemu remmina samba spotify stacer termite tigervnc trayer vifm volumeicon fontmatrix sxiv hplip python-pyqt5 rpcbind sane xsane python-reportlab python-pillow libusb hplip-plugin alsa-utils vi 0ad alsa-lib alsa-plugins alsa-utils android-studio-canary arc-gtk-theme arc-icon-theme batbash-completion awk checkupdates+aur cmake code gparted kdeconnect make lxappearance obconf obmenu tar mbsync-git mailspring
+paru -S alacritty audacity blender brave-bin cheese deadbeef discord emacs exa gimp htop kdenlive libnotify libreoffice-fresh lutris mpv nano neofetch nitrogen notepadqq notifications-daemon neovim picom polkit qbittorent steam sudo udisks2 vim virt-manager virtualbox vlc wine-staging winetricks multimc multimc-curseforge librewolf-bin snapd kvantum xterm pcmanfm mu kitty appimagelauncher ripgrep fd atom blueman bullshit chrome-remote-desktop chromium checkupdates-aur emby-theater-bin feh firefox fish geary heroic-games-launcher-bin minigalaxy minecraft-launcher network-manager-applet networkmanager newsflash nextcloud-client obs-studio openssh openvpn plex-media-player playerctl playonlinux pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-qt qemu remmina samba spotify stacer termite tigervnc trayer vifm volumeicon fontmatrix sxiv hplip python-pyqt5 rpcbind sane xsane python-reportlab python-pillow libusb hplip-plugin alsa-utils vi 0ad alsa-lib alsa-plugins alsa-utils android-studio-canary arc-gtk-theme arc-icon-theme batbash-completion awk checkupdates+aur cmake code gparted kdeconnect make lxappearance obconf obmenu tar mbsync-git mailspring llvm direnv-bin rtags
 
 # Archer T6E
 sudo pacman -S $(pacman -Qsq "^linux" | grep "^linux[0-9]*[-rt]*$" | awk '{print $1"-headers"}' ORS=' ')
@@ -34,7 +34,7 @@ tar xf pulseway_x64.pkg.tar.xz -C ./pulseway
 cd pulseway
 sudo cp -r etc/* /etc/
 sudo cp -r var/* /var/
-sudo cp -r usr/ /usr/
+sudo cp -r usr/* /usr/
 sudo systemctl enable --now pulseway
 sudo cp /etc/pulseway/config.xml.sample /etc/pulseway/config.xml
 sudo systemctl restart pulseway
@@ -57,7 +57,6 @@ cd ~/
 
 # Installing BadWolf
 cd ~/sourcecode
-wget https://hacktivis.me/releases/badwolf-1.0.3.tar.gz
 tar xfzv badwolf*.tar.gz
 mv badwolf*/ badwolf
 cd badwolf
@@ -73,26 +72,14 @@ sudo make install
 cd ~/
 
 # Installing rust-lang and needed tools
-paru -S rustup
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 rustup toolchain add nightly
 cargo +nightly install racer
 rustup component add rust-src
-paru -S jdk-openjdk python pmbootstrap android-emulator-canary go nodejs npm eclipse-rust
-
-# Installing doom emacs
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
-
-# Setting up dotfiles
-git clone https://www.gitlab.com/SuperMrDoggyDog/dotfiles.git ~/DotFiles
-cd ~/DotFiles
-cp .bashrc ~/
-cp -r .doom.d/ ~/.doom
-cp -r .config/ ~/.config/
-cp -r .local   ~/.local
-cp -r .xmonad ~/.local
+paru -S jdk-openjdk python pmbootstrap android-emulator-canary go nodejs npm eclipse-rust hlint cabal-install rust-analyzer ktlint marked python-isort python-pipenv python-nose python-pytest shellcheck stylelint js-beautify
 
 # Cloning git repos
+git clone https://www.gitlab.com/SuperMrDoggyDog/dotfiles.git ~/DotFiles
 git clone https://www.gitlab.com/dwt1/dotfiles.git ~/DTDotFiles
 git clone https://www.gitlab.com/SuperMrDoggyDog/wallpapers.git ~/Wallpapers
 git clone https://www.gitlab.com/dwt1/wallpapers.git ~/DTWallpapers
--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git add "./*"
-
-read -p "Please enter commit message: " message
-
-git commit -m "$message"
-
-git push all
